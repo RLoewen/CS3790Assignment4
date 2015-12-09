a849d288347ba3b70b7528de602a66800b24496b
Ruben Loewen <Ruben@Rubens-MacBook-Air.local>
2015-12-08 23:40:12 -0700

    initial upload

---
+++ MemAllocator.c
@@ -0,0 +1,68 @@
+#define MIN_LEASE 40
+#define MAX_LEASE 70
+#define MIN_SIZE 50
+#define MAX_SIZE 350
+#define TIME_LIMIT 1000
+#define MEMORY_SIZE 1000
+#define REQUEST_INTERVAL 10
+#include <stdio.h>
+
+typedef struct REQUEST_NODE {
+	int leaseLength;
+	int nodeSize;
+}REQUEST_NODE;
+
+struct range {
+	int start;		// the starting address of the range
+	int size;		// size of the range
+}
+
+struct freeNode {
+	struct range hole;
+	struct freeNode *next;	// pointer to next node
+}
+
+struct allocNode {
+	struct range allocated;
+	int leaseExpiry; 	// time at which this block will be returned to free list
+	struct allocNode *next;	//pointer to next node;
+}
+
+typedef struct filesystem{
+
+	DIRECTORY d;
+	FAT F;
+	BLOCK B[FATSIZE];
+}filesystem;
+
+REQUEST_NODE rNode;	// create a rNode
+
+int generateRequest(){
+
+	rNode.nodeSize = (rand() % (MAX_SIZE - MIN_SIZE) + MIN_SIZE);
+	rNode.leaseLength = (rand() % (MAX_LEASE - MIN_LEASE) + MIN_LEASE);
+
+	return 0;
+}
+
+
+int main () {
+
+	long clock = 0;
+    while (++clock != TIME_LIMIT){
+
+    	
+      /* generate a memory request */
+    	if(clock % 10 == 0) generateRequest();
+    	printf(stderr,"requestNode size: \n",rNode.nodeSize);
+    	printf(stderr,"requestNode lease length: \n",rNode.leaseLength);
+      /* see if lease has expired  */
+
+
+    }
+    until ( ++clock == TIME_LIMIT);
+
+    return 0;
+
+}
+   
---
+++ readme.txt
@@ -0,0 +1,14 @@
+
+
+CS3790 Assignment 4 file system simmulation
+
+Author: Ruben Loewen, 5010926
+
+
+compile when in this directory with: gcc -o MemAllocator MemAllocator.c
+
+
+Changelog
+
+1.0	If this is all you are reading then I procrastinated/forgot to check yoour deadline and posted this up in an attempt to sneak in more work through the night.
+
